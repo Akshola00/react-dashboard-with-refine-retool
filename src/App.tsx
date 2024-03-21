a0fff0f3a2982f403abb77a0b4b31f8cb4c8b3ee
Akshola00 <akinniyishola07@gmail.com>
2024-03-21 09:48:59 +0100

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import {  Refine, WelcomePage } from "@refinedev/core";
+import {  Authenticated, Refine, WelcomePage } from "@refinedev/core";
 import { dataProvider } from "./Providers/data-providers";
 import { ShowProduct } from "./Pages/ShowProducts";
 import { EditProduct } from "./Pages/EditProducts";
+import { CreateProduct } from "./Pages/CreateProducts";
+import { ListProducts } from "./Pages/ListProducts";
+import { authProvider } from "./Providers/auth-providers";
 // import { ShowProducts } from "./Pages/ShowProduct";
 
 
 function App() {
   return (
-   <Refine dataProvider={dataProvider} > 
+   <Refine dataProvider={dataProvider} authProvider={authProvider} > 
     {/* <WelcomePage /> */}
     {/* <ShowProduct/> */}
-    <EditProduct />
+    {/* <EditProduct /> */}
+    {/* <CreateProduct /> */}
+    <Authenticated key="protected" fallback={<div>please login</div>} >
+      <ListProducts/>
+    </Authenticated>
+    {/* <ListProducts /> */}
    </Refine>
   );
 }
